019e495e10c5610ea4e8b932b1adbcd73b370298
Z User <z@container>
2026-04-19 02:00:34 +0000

    8f4293b2-4cbc-46c4-bbf9-c8c4e2f1407b

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.xuexidaoren.app',
+  appName: '学习小达人',
+  webDir: 'out'
+};
+
+export default config;
