7c5fcee0e3817d85d050bf7bb7e25ac6d3320ec6
Z User <z@container>
2026-05-19 01:43:58 +0000

    fix: 修复APK闪退问题 - 改用Capacitor BridgeActivity
    - 将MainActivity从自定义WebView改为继承Capacitor的BridgeActivity
    - 添加网络安全配置(network_security_config.xml)允许本地连接
    - AndroidManifest添加networkSecurityConfig和usesCleartextTraffic
    - 更新设置页面APK大小为30MB

--- android/app/src/main/java/com/zhishixiaoyongshi/app/MainActivity.java
+++ android/app/src/main/java/com/zhishixiaoyongshi/app/MainActivity.java
@@ -1,40 +1,11 @@
 package com.zhishixiaoyongshi.app;
 
 import android.os.Bundle;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.webkit.WebSettings;
-import android.view.KeyEvent;
-import androidx.appcompat.app.AppCompatActivity;
+import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends AppCompatActivity {
-
-    private WebView webView;
-
+public class MainActivity extends BridgeActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        webView = new WebView(this);
-        setContentView(webView);
-
-        WebSettings settings = webView.getSettings();
-        settings.setJavaScriptEnabled(true);
-        settings.setDomStorageEnabled(true);
-        settings.setAllowFileAccess(true);
-        settings.setAllowContentAccess(true);
-
-        webView.setWebViewClient(new WebViewClient());
-
-        webView.loadUrl("file:///android_asset/public/index.html");
-    }
-
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
-            webView.goBack();
-            return true;
-        }
-        return super.onKeyDown(keyCode, event);
     }
 }
